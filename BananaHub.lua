d51cfa9c3af70f5a3ac8f4b2bbf9e4283758032d
Iwxz_ <ngtrtinh0812@gmail.com>
2025-10-05 12:24:22 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
     if getgenv().betabss then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/hlamx/huhu/master/bssrewrite-obfuscated.lua"))()
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 or UniverseID == 994732206 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/minigayio/BananaCat/main/BF-BananaCat.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/minigayio/BananaCat/refs/heads/main/BF-BananaCat.lua"))()
 elseif UniverseID == 7709344486 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/minigayio/BananaCat/refs/heads/main/BananaCat-Brainrot.lua"))()
-end
+    end
